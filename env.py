7addf253722964f1018ecc5fc8bdcef74ec5871b
kentaro2003kojima@gmail.com <kentaro2003kojima@gmail.com>
2025-03-03 16:14:48 +0900

    [add] TCGEnv_v2

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ from pettingzoo import ParallelEnv
 import numpy as np
 from numpy import random as rnd
 import copy
-
+import random
 MAX_STEP = 5000
 
 class TCGEnv(ParallelEnv):
@@ -56,6 +56,7 @@ class TCGEnv(ParallelEnv):
         self.decks = {'agent_0': copy.deepcopy(deck), 'agent_1': copy.deepcopy(deck)}
         self.trancated = {agent: False for agent in self.agents}
         self.steps = 0
+        self.agent_1_mode = "aggro" if random.randint(2) == 0 else "control"
     
     def create_observation(self):
 
@@ -317,7 +318,183 @@ class TCGEnv(ParallelEnv):
                 if card_index != -1:
                     self.hands[agent][card_index] = card
         return False
-            
+    
+class TCGEnv_v2(gym.Env):
+    metadata = {"render.modes": ["human"]}
+    CARD_ATTACK = 0
+    CARD_HEALTH = 1
+    CARD_PP = 2
+    CARD_ABILITY = 3
+    card_map = {
+        'card_0': [4, 4, 1, 0],
+        'card_1': [2, 2, 2, 0],
+        'card_2': [3, 3, 3, 0],
+        'card_3': [4, 3, 4, 0],
+        'card_4': [5, 4, 5, 0],
+        'card_5': [2, 2, 2, 1],
+        'card_6': [2, 3, 3, 1],
+        'card_7': [1, 1, 1, 4],
+        'card_8': [1, 3, 2, 4],
+        'card_9': [2, 1, 2, 5],
+        'card_10': [3, 1, 3, 5],
+        'card_11': [1, 2, 2, 3],
+        'card_12': [2, 3, 3, 3],
+        'card_13': [1, 1, 1, 2],
+        'card_14': [1, 1, 5, 2],
+    }
+    rewards_map = {
+        'punish': 0.0,
+        'reward': 0.0,
+        'win': 1.0,
+        'lose': -1.0
+    }
+
+    def __init__(self):
+        self.action_space = Discrete(40)
+        self.observation_space = Box(low=0, high=30, shape=(67, ), dtype=np.uint16)
+        self.LeadingPlayer = "agent_0" if random.randint(2) == 0 else "agent_1"
+        self.SecondPlayer = "agent_0" if self.LeadingPlayer == "agent_1" else "agent_1"
+        self.turn = {self.LeadingPlayer: 1, self.SecondPlayer: 0}
+        self.health = {self.LeadingPlayer: 20, self.SecondPlayer: 20}
+        self.PP = {self.LeadingPlayer: 1, self.SecondPlayer: 0}
+        self.hands = {self.LeadingPlayer: [[0 for _ in range(4)] for _ in range(9)], self.SecondPlayer: [[0 for _ in range(4)] for _ in range(9)]}
+        self.fields = {self.LeadingPlayer: [[0 for _ in range(2)] for _ in range(5)], self.SecondPlayer: [[0 for _ in range(2)] for _ in range(5)]}
+        self.attackable = {self.LeadingPlayer: [0 for _ in range(5)], self.SecondPlayer: [0 for _ in range(5)]}
+        deck = []
+        for i in range(15):
+            deck += [self.card_map[f'card_{i}'] for _ in range(2)]
+        self.decks = {self.LeadingPlayer: copy.deepcopy(deck), self.SecondPlayer: copy.deepcopy(deck)}
+        self.trancated = {self.LeadingPlayer: False, self.SecondPlayer: False}
+        
+    def create_observation(self):
+        concated_obs = []
+        concated_obs += [self.health["agent_0"], self.health["agent_1"]]
+        concated_obs += [self.PP["agent_0"], self.PP["agent_1"]]
+        concated_obs += flatten_list(self.hands["agent_0"])
+        concated_obs += flatten_list(self.fields["agent_0"])
+        concated_obs += flatten_list(self.fields["agent_1"])
+        concated_obs += self.attackable["agent_0"]
+        concated_obs += [len(self.decks["agent_0"]), len(self.decks["agent_1"])]
+        return np.array(concated_obs).astype(np.uint16)
+        
+    def step(self, action):
+        if 0 <= action <= 8:
+            obs, reward, done, info = self.play("agent_0", action)
+            return obs, reward, done, self.trancated
+        elif action == 39:
+            obs, reward, done, info = self.end_turn("agent_0")
+            return obs, reward, done, self.trancated
+        else:
+            obs, reward, done, info = self.attack("agent_0", (action - 9) // 6, (action - 9) % 6)
+            return obs, reward, done, self.trancated
+    
+    def reset(self):
+        self.LeadingPlayer = "agent_0" if random.randint(2) == 0 else "agent_1"
+        self.SecondPlayer = "agent_0" if self.LeadingPlayer == "agent_1" else "agent_1"
+        self.turn = {self.LeadingPlayer: 1, self.SecondPlayer: 0}
+        self.health = {self.LeadingPlayer: 20, self.SecondPlayer: 20}
+        self.PP = {self.LeadingPlayer: 1, self.SecondPlayer: 0}
+        self.hands = {self.LeadingPlayer: [[0, 0, 0, 0] for _ in range(9)], self.SecondPlayer: [[0, 0, 0, 0] for _ in range(9)]}
+        self.fields = {self.LeadingPlayer: [[0, 0] for _ in range(5)], self.SecondPlayer: [[0, 0] for _ in range(5)]}
+        self.attackable = {self.LeadingPlayer: [0 for _ in range(5)], self.SecondPlayer: [0 for _ in range(5)]}
+        deck = []
+        for i in range(15):
+            deck += [self.card_map[f'card_{i}'] for _ in range(2)]
+        self.decks = {self.LeadingPlayer: copy.deepcopy(deck), self.SecondPlayer: copy.deepcopy(deck)}
+        self.draw_n(self.LeadingPlayer, 5)
+        self.draw_n(self.SecondPlayer, 5)
+        return self.create_observation()
+    
+    def play(self, agent, card_index):
+        switch_agent = 'agent_0' if agent == 'agent_1' else 'agent_1'
+        if self.hands[agent][card_index][self.CARD_HEALTH] == 0:
+            observation = self.create_observation()
+            return observation, self.rewards_map['punish'], False, {}
+        card_info = self.hands[agent][card_index]
+        if card_info[self.CARD_PP] > self.PP[agent]:
+            observation = self.create_observation()
+            return observation, self.rewards_map['punish'], False, {}
+        field_index = self.find_empty_field(agent)
+        if field_index == -1:
+            observation = self.create_observation()
+            return observation, self.rewards_map['punish'], False, {}
+        done = False
+        self.PP[agent] -= card_info[self.CARD_PP]
+        self.hands[agent][card_index] = [0, 0, 0, 0]
+        self.fields[agent][field_index] = [card_info[self.CARD_ATTACK], card_info[self.CARD_HEALTH]]
+        done, reward = self.activate_ability(agent, card_info, field_index=field_index)
+        obs = self.create_observation()
+        return obs[agent], reward, done, {}
+    
+    def attack(self, agent, attacker_index, attacked_index):
+        switch_agent = 'agent_0' if agent == 'agent_1' else 'agent_1'
+        attacked_destruction = False
+        attacker_destruction = False
+        if self.fields[agent][attacker_index][self.CARD_HEALTH] == 0:  #エージェントが指定した自盤面にカードが存在しなかった場合
+            observation = self.create_observation()
+            return observation, {agent: self.rewards_map['punish'], switch_agent:0.0}, {agent: False, switch_agent: False}, {agent: {}, switch_agent: {}}
+        if attacked_index <= 4 and self.fields[switch_agent][attacked_index][self.CARD_HEALTH] == 0:  #エージェントが指定した相手盤面にカードが存在しなかった場合
+            observation = self.create_observation()
+            return observation, {agent: self.rewards_map['punish'], switch_agent:0.0}, {agent: False, switch_agent: False}, {agent: {}, switch_agent: {}}
+        if self.attackable[agent][attacker_index] == 0:  #エージェントが指定した自盤面のカードが攻撃可能でなかった場合
+            observation = self.create_observation()
+            return observation, {agent: self.rewards_map['punish'], switch_agent:0.0}, {agent: False, switch_agent: False}, {agent: {}, switch_agent: {}}
+        if attacked_index <= 4:  #相手盤面のカードを攻撃するときの相手カードの処理
+            self.fields[switch_agent][attacked_index][self.CARD_HEALTH] -= self.fields[agent][attacker_index][self.CARD_ATTACK]
+        elif attacked_index == 5:  #相手プレイヤーを攻撃するときの処理
+            self.health[switch_agent] -= self.fields[agent][attacker_index][self.CARD_ATTACK]
+        if attacked_index <= 4:  #相手盤面のカードを攻撃するときの自カードの処理
+            self.fields[agent][attacker_index][self.CARD_HEALTH] -= self.fields[switch_agent][attacked_index][self.CARD_ATTACK]
+        elif attacked_index == 5:  #相手プレイヤーを攻撃するときの自カードの処理
+            pass
+
+        if attacked_index <= 4 and self.fields[switch_agent][attacked_index][self.CARD_HEALTH] <= 0:  #相手盤面のカードが破壊された場合
+            self.fields[switch_agent][attacked_index] = [0, 0]
+            self.attackable[switch_agent][attacked_index] = 0
+            attacked_destruction = True
+        if self.fields[agent][attacker_index][self.CARD_HEALTH] <= 0:  #自盤面のカードが破壊された場合
+            self.fields[agent][attacker_index] = [0, 0]
+            self.attackable[agent][attacker_index] = 0
+            attacker_destruction = True
+        self.attackable[agent][attacker_index] = 0
+        done = False
+        if attacked_index <= 4:
+            if attacked_destruction and attacker_destruction:
+                reward = self.rewards_map['reward']
+            elif attacked_destruction:
+                reward = self.rewards_map['reward']
+            elif attacker_destruction:
+                reward = 0.0
+            else:
+                reward = self.rewards_map['reward']
+        else:
+            if self.health[switch_agent] <= 0:
+                reward = self.rewards_map['reward']
+                done = True
+            else:
+                reward = self.rewards_map['reward'] * self.fields[agent][attacker_index][self.CARD_ATTACK]
+        observation = self.create_observation()
+        return observation[agent], reward, done, {}
+    
+    def end_turn(self, agent):
+        switch_agent = 'agent_0' if agent == 'agent_1' else 'agent_1'
+        self.turn[switch_agent] += 1
+        self.PP[switch_agent] = min(self.turn[switch_agent], 8)
+        done = self.draw_n(switch_agent, 1)
+        if done:
+            observation = self.create_observation()
+            return observation, {agent: self.rewards_map['win'], switch_agent:self.rewards_map['lose']}, {agent: done, switch_agent: done}, {agent: {}, switch_agent: {}}
+        for i in range(5):
+            if self.fields[switch_agent][i][self.CARD_HEALTH] != 0:
+                self.attackable[switch_agent][i] = 1
+        if switch_agent == "agent_1":
+            self.agent_1_play(self.agent_1_mode)
+        observation = self.create_observation()
+        return observation, {agent: 0.0, switch_agent:0.0}, {agent: False, switch_agent: False}, {agent: {}, switch_agent: {}}
+
+    def agent_1_play(self, mode):
+        pass
+    
 def base_n(num_10,n):
     str_n = ''
     while num_10:
